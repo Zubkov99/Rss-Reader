1fece14cfc76d1cebb155a3ed2c0b2d020d97f30
DimaZ <dimazubkov99@gmail.com>
2022-03-26 21:18:55 +0300

    done 3 step

---
+++ src/localText.js
@@ -0,0 +1,10 @@
+const ru = {
+  translation: {
+    validFeedback: {
+      invalid: 'Невальдная, либо ранее загруженная ссылка',
+      valid: 'RSS загружен',
+    },
+  },
+};
+
+export default ru;
--- src/render.js
+++ src/render.js
@@ -1,12 +1,32 @@
 /* eslint-disable no-undef */
+import i18next from 'i18next';
+import ru from './localText.js';
+
+const i18nextInstance = i18next.createInstance();
+
+i18nextInstance.init({
+  lng: 'ru',
+  debug: true,
+  resources: {
+    ru,
+  },
+});
+
 const render = (state) => {
   const form = document.querySelector('.rss-form');
   const input = document.querySelector('#url-input');
+  const feedback = document.querySelector('.feedback');
   const validClass = 'is-invalid';
 
   if (state.validFlug === false) {
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.textContent = i18nextInstance.t('validFeedback.invalid');
     input.classList.add(validClass);
   } else {
+    feedback.textContent = i18nextInstance.t('validFeedback.valid');
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
     input.classList.remove(validClass);
     form.reset();
   }

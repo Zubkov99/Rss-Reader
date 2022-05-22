e204e2fdd4c7351c19f98808d0b3fd405fa29828
DimaZ <dimazubkov99@gmail.com>
2022-05-22 23:02:24 +0300

    fix text

--- src/localText.js
+++ src/localText.js
@@ -1,9 +1,9 @@
 const ru = {
   translation: {
     validFeedback: {
-      invalid: 'Невальдная ссылка',
-      valid: 'RSS загружен',
-      notUnique: 'Ссылка уже была загружена ранее',
+      invalid: 'Ссылка должна быть валидным URL',
+      valid: 'RSS успешно загружен',
+      notUnique: 'RSS уже существует',
       networkError: 'Ошибка сети',
       waitResponse: 'Ждем ответа сети',
     },
